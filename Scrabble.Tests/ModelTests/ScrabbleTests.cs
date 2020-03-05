65ca5b5d462bbcf492e116a3d23a261c05e1c93b
Fatma Dogan and Kristina Hengster Tintor <kristina@>
2020-03-05 10:41:53 -0800

    pass test for Dictionary

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Scrabble.Models;
 using System;
+using System.Collections.Generic;
 
 namespace Scrabble.Tests
 {
@@ -20,6 +21,23 @@ namespace Scrabble.Tests
    {
      Game newGame = new Game("hi");
      Assert.AreEqual("hi", newGame.Word);
+   }
+
+   [TestMethod]
+   public void MakeArray_ConvertsStringToArray_Array()
+   {
+     Game newGame = new Game("hello");
+     char[] result = {'h', 'e', 'l', 'l', 'o'};
+     CollectionAssert.AreEqual(result, newGame.MakeArray("hello"));
+   }
+
+   [TestMethod]
+   public void GetAll_CreateEmptyDictionary_Dictionary()
+   {
+    //  Game newGame = new Game("hello");
+     Dictionary<int, char[]> _gameDictionary = new Dictionary <int, char[]>(){};
+     Dictionary<int, char[]> result = Game.GetAll();
+     CollectionAssert.AreEqual(result, _gameDictionary);
    }
   }
 }
